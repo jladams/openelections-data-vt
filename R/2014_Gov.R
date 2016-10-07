0842391e95a44e20fbae6e6fbb95a742cde4ea4d
jladams <adamsjlp@gmail.com>
2016-10-06 22:13:42 -0400

    add house processing

--- R/2014_Gov.R
+++ R/2014_Gov.R
@@ -8,7 +8,7 @@ prime_libu <- process_primary("./data/results_raw/2014/gov/VT Elections Database
 prime_pro <- process_primary("./data/results_raw/2014/gov/VT Elections Database__2014_Governor_Progressive_Primary_including_precincts.csv", "Governor", "Progressive")
 
 # Combine processed primary data
-primary_2014 <- rbind(prime_dem,prime_libu,prime_pro,prime_rep)
+primary <- rbind(prime_dem,prime_libu,prime_pro,prime_rep)
 
 # Process general election data
-general_2014 <- process_general("./data/results_raw/2014/gov/VT Elections Database__2014_Governor_General_Election_including_precincts.csv", "Governor")
+general <- process_general("./data/results_raw/2014/gov/VT Elections Database__2014_Governor_General_Election_including_precincts.csv", "Governor")
---
+++ R/2014_house.R
@@ -0,0 +1,15 @@
+# Load required packages and functions
+source("./R/prep.R")
+
+# Process raw primary data per party
+prime_rep <- process_primary("./data/results_raw/2014/house/VT Elections Database__2014_U_S_House_Republican_Primary_including_precincts.csv", "U.S. House", "Republican")
+prime_dem <- process_primary("./data/results_raw/2014/house/VT Elections Database__2014_U_S_House_Democratic_Primary_including_precincts.csv", "U.S. House", "Democratic")
+prime_libu <- process_primary("./data/results_raw/2014/house/VT Elections Database__2014_U_S_House_Liberty_Union_Primary_including_precincts.csv","U.S. House", "Liberty Union")
+prime_pro <- process_primary("./data/results_raw/2014/house/VT Elections Database__2014_U_S_House_Progressive_Primary_including_precincts.csv", "U.S. House", "Progressive")
+
+# Combine processed primary data
+primary <- rbind(prime_dem,prime_libu,prime_pro,prime_rep)
+
+# Process general election data
+general <- process_general("./data/results_raw/2014/house/VT Elections Database__2014_U_S_House_General_Election_including_precincts.csv", "U.S. House")
+
